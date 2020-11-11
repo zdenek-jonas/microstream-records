2669d5c91e50f2425ca931690c970bae9a4b5bd2
Zdenek Jonas <z.jonas@microstream.one>
2020-11-11 13:47:48 +0100

    first commit

---
+++ src/main/java/test/microstream/data/PersonRecord.java
@@ -0,0 +1,6 @@
+package test.microstream.data;
+
+public record PersonRecord(String firstName, String lastName) {
+
+}
+
---
+++ src/test/java/test/microstream/RecordTest.java
@@ -0,0 +1,49 @@
+package test.microstream;
+
+import one.microstream.storage.types.EmbeddedStorage;
+import one.microstream.storage.types.EmbeddedStorageManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import test.microstream.data.PersonRecord;
+
+import java.nio.file.Path;
+
+public class RecordTest {
+
+    @TempDir
+    Path location;
+
+
+
+    @Test
+    public void saveRecordTest() {
+        PersonRecord personRecord = new PersonRecord("Maria", "Lukasova");
+
+        EmbeddedStorageManager storage = EmbeddedStorage.start(personRecord, location );
+        storage.shutdown();
+
+        PersonRecord secondRecord = new PersonRecord("Kamila", "Pazourkova");
+        storage = EmbeddedStorage.start(secondRecord, location);
+
+        Assertions.assertEquals("Maria", secondRecord.firstName());
+
+    }
+
+    @Test
+    public void declaringClassTest() {
+        PersonRecord personRecord = new PersonRecord("Maria", "Lukasova");
+
+        EmbeddedStorageManager storage = EmbeddedStorage.start(personRecord, location );
+        storage.shutdown();
+
+        PersonRecord secondRecord = new PersonRecord("Kamila", "Pazourkova");
+        storage = EmbeddedStorage.start(secondRecord, location);
+
+        Class<?> f = secondRecord.getClass().getDeclaringClass();
+
+        Assertions.assertEquals("Maria", secondRecord.firstName());
+
+    }
+
+}
